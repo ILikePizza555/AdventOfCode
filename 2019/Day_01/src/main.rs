d5fd8ac50e93c634af1bb3ff87dab81efa0e318a
Izzy <avrisaac555@gmail.com>
2019-12-03 20:50:31 -0800

    Finished day 1

--- 2019/Day_01/src/main.rs
+++ 2019/Day_01/src/main.rs
@@ -1,13 +1,39 @@
-fn part1(input: &str) -> u64 {
-    return input
+fn get_input_masses() -> Vec<u64> {
+    return include_str!("input_1.txt")
         .trim()
         .split("\n")
         .map(|x| str::parse::<u64>(x).unwrap())
-        .map(|x| x / 3 - 2)
-        .sum::<u64>();
+        .collect::<Vec<u64>>();
+}
+
+fn mass_to_fuel_total(mass: u64) -> u64 {
+    let mut sum: u64 = 0;
+    let mut fuel: u64 = mass;
+
+    return loop {
+        fuel = if fuel > 6 { fuel / 3 - 2 } else { 0 };
+        sum += fuel;
+
+        if fuel == 0 {
+            break sum;
+        }
+    }
 }
 
 fn main() {
-    let input_str =  include_str!("input_1.txt");
-    println!("Result: {}", part1(input_str));
+    let input_masses = get_input_masses();
+
+    let module_fuel = input_masses
+        .iter()
+        .cloned()
+        .map(|x| x / 3 - 2)
+        .sum::<u64>();
+
+    let total_fuel = input_masses
+        .iter()
+        .cloned()
+        .map(mass_to_fuel_total)
+        .sum::<u64>();
+
+    println!("Module fuel: {}; Total fuel: {}", module_fuel, total_fuel);
 }
