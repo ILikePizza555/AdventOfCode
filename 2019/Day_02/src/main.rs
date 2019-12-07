a446b98057c273adb6abe9b3db90ca1c7f8a7678
Izzy <avrisaac555@gmail.com>
2019-12-07 15:57:06 -0800

    Day 2 starting code

--- 2019/Day_02/src/main.rs
+++ 2019/Day_02/src/main.rs
@@ -1,3 +1,12 @@
+fn get_input_code() -> Vec<u8> {
+    return include_str!("input1.csv")
+        .trim()
+        .split(",")
+        .map(|x| str::parse::<u8>(x).unwrap())
+        .collect::<Vec::<u8>>();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input_code = get_input_code();
+    println!("Got {} codes", input_code.len());
 }
