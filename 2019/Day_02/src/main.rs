0c24ca07310f273f9930be5de681622370d1dcfe
Izzy <avrisaac555@gmail.com>
2019-12-07 16:36:21 -0800

    MachineState code

--- 2019/Day_02/src/main.rs
+++ 2019/Day_02/src/main.rs
@@ -1,3 +1,27 @@
+struct MachineState
+{
+    halt: bool,
+    memory: Vec<u8>,
+    instruction_pointer: usize
+}
+
+impl MachineState {
+    fn set_instruction_pointer(&mut self, to: usize) {
+        if to > self.memory.len() {
+            panic!("New instruction pointer {} outside of memory range {}", to, self.memory.len());
+        }
+        self.instruction_pointer = to;
+    }
+}
+
+fn make_machine_state(memory: Vec<u8>) -> MachineState {
+    return MachineState {
+        halt: false,
+        memory: memory,
+        instruction_pointer: 0
+    }
+}
+
 fn get_input_code() -> Vec<u8> {
     return include_str!("input1.csv")
         .trim()
@@ -7,6 +31,6 @@ fn get_input_code() -> Vec<u8> {
 }
 
 fn main() {
-    let input_code = get_input_code();
-    println!("Got {} codes", input_code.len());
+    let machine_state = make_machine_state(get_input_code());
+    println!("Got {} codes", machine_state.memory.len());
 }
