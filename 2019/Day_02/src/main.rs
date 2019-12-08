98103096f879118b24e71f8e33d422172dbe1147
Izzy <avrisaac555@gmail.com>
2019-12-07 18:57:21 -0800

    Added step function

--- 2019/Day_02/src/main.rs
+++ 2019/Day_02/src/main.rs
@@ -48,6 +48,46 @@ impl<TMem: Integer> MachineState<TMem> {
     fn read_memory(&self, i: usize, n: usize) -> &[TMem] {
         return &self.memory[i..i + n];
     }
+
+    fn step(self) -> Result<MachineState<TMem>, String> {
+        if self.halt == true {
+            return Err("Machine is halted.");
+        }
+
+        let opcode = self.memory[self.instruction_pointer];
+        let a = self.memory[self.instruction_pointer + 1];
+        let b = self.memory[self.instruction_pointer + 2];
+        let loc = self.memory[self.instruction_pointer + 3];
+
+        let mut new_memory = self.memory;
+
+        if opcode == 1 {
+            // Addition
+            new_memory[loc] = a + b;
+            
+            return Ok(MachineState {
+                halt: self.halt,
+                memory: new_memory,
+                instruction_pointer: self.instruction_pointer + 4
+            });
+        }
+        else if opcode == 2 {
+            // Multiplication
+            new_memory[loc] = a * b;
+
+            return Ok(MachineState {
+                halt: self.halt,
+                memory: new_memory,
+                instruction_pointer: self.instruction_pointer + 4
+            });
+        }
+        else if opcode == 99 {
+            return Ok(self.halt());
+        }
+        else {
+            return Err("Invalid opcode {} at memory location {}", opcode, self.instruction_pointer);
+        }
+    }
 }
 
 fn make_machine_state<TMemSize: Integer>(memory: Vec<TMemSize>) -> MachineState<TMemSize> {
@@ -67,6 +107,10 @@ fn get_input_code() -> Vec<i32> {
 }
 
 fn main() {
-    let machine_state = make_machine_state(get_input_code());
-    println!("Got {} codes", machine_state.memory.len());
+    let machine_state = make_machine_state(get_input_code())
+        .write_memory(1, 12)
+        .and_then(|m| m.write_memory(2, 2))
+        .expect("Error: ");
+    
+    println!("Memory: {:?}", machine_state.memory);
 }
